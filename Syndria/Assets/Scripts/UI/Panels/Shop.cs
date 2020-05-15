fd84c5352a8461692bebd856f8fe316534cebc68
unknown <sahyno1996@gmail.com>
2020-05-15 19:18:27 +0200

    minor visual changes

---
+++ Syndria/Assets/Scripts/UI/Panels/Shop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : UIPanel
+{
+    public Button closeBtn;
+
+    void Start()
+    {
+        closeBtn.onClick.AddListener(() =>
+        {
+            Close();
+        });  
+    }
+
+    void Update()
+    {
+        
+    }
+}
